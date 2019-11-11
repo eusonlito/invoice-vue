5dd3750df1dcfcbd3edc473fcca8307c6e7e01e4
Lito <lito@eordes.com>
2019-11-11 23:02:17 +0100

    Updated roadmap

--- src/views/pages/info/Roadmap.vue
+++ src/views/pages/info/Roadmap.vue
@@ -8,12 +8,17 @@
 
                 <p class="mb-5 text-center">El camino a seguir será (más o menos por prioridad).</p>
 
-                <vs-list>
-                    <vs-list-item title="Exportador de facturas/clientes/productos en formato JSON"></vs-list-item>
+                <vs-list class="mb-5">
                     <vs-list-item title="Firma digital integrada para el PDF de factura"></vs-list-item>
                     <vs-list-item title="Personalización del PDF de factura mediante subida de SCSS"></vs-list-item>
                     <vs-list-item title="Logo de empresa para PDF de factura"></vs-list-item>
                 </vs-list>
+
+                <p class="text-center">
+                    Puedes seguir la evolución del proyecto en
+                    <a href="https://github.com/eusonlito/invoice-vue" target="_blank">https://github.com/eusonlito/invoice-vue</a>
+                    y <a href="https://github.com/eusonlito/invoice-api" target="_blank">https://github.com/eusonlito/invoice-api</a>
+                </p>
             </vx-card>
 
             <div class="text-center">
